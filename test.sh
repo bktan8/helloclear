e60691ca59e181b5bd78ec6903d131e9eee18d03
Bun K Tan <bun.k.tan@intel.com>
2024-11-05 00:06:19 +0000

    Test
    Signed-off-by: Bun K Tan <bun.k.tan@intel.com>

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+wget https://kernel.ubuntu.com/mainline/v6.8.11/amd64/linux-headers-6.8.11-060811-generic_6.8.11-060811.202405251535_amd64.deb
+wget https://kernel.ubuntu.com/mainline/v6.8.11/amd64/linux-headers-6.8.11-060811_6.8.11-060811.202405251535_all.deb
+wget https://kernel.ubuntu.com/mainline/v6.8.11/amd64/linux-image-unsigned-6.8.11-060811-generic_6.8.11-060811.202405251535_amd64.deb
+wget https://kernel.ubuntu.com/mainline/v6.8.11/amd64/linux-modules-6.8.11-060811-generic_6.8.11-060811.202405251535_amd64.deb
